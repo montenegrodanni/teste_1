3c38f1d18d282a96ffd23d983ab6c557a8116231
Michel Bittencourt <59713262+michel-bittencourt@users.noreply.github.com>
2022-04-15 16:24:50 -0300

    resetando

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-include_once("templates/cabecalho.php");
-?>
-
-<main>
-    <h2><a href="contatos.php">CLIQUE AQUI APRA IR PARA CONTATOS</a></h2>
-</main>
-
-<?php
-include_once("templates/rodape.php");
+    echo "TESTE";
 ?>
